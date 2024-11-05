6bc095eb08b9874b3169fcd5704e927238a3f10c
Taaftt <alejandro.andresvg@gmail.com>
2024-11-05 16:56:38 -0300

    Update music.py

--- music.py
+++ music.py
@@ -9,3 +9,15 @@ df = pd.read_csv("top 100 streamed_songs.csv")
 st.title("Mejores 100 canciones de spotify")
 #descripcion
 st.write("Explora los mejores temas")
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+# Mostrar los primeros registros del DataFrame
+st.subheader("Vista previa de los datos")
+st.write(df.head())
+
+
+
